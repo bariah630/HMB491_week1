cb3a5b63bab7c033dd03520fea33813f95a1279a
Bariah Shamshad <bariah06@Bariahs-MacBook-Air.local>
2025-09-25 15:07:34 -0400

    Complete week 1 plot and generate scatterplot

--- week1_plot.py
+++ week1_plot.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# TODO: load the dataset as pandas dataframe
+# Load the dataset as pandas dataframe
+df_teeth = pd.read_csv("mammal_teeth.csv")
 
 plt.figure(figsize=(5, 10)) # set figure size
 plt.scatter(x=df_teeth['Top incisors'],
-y=df_teeth['MAMMAL']) # set figure x, y axis
+            y=df_teeth['MAMMAL']) # set figure x, y axis
 plt.gca().xaxis.set_visible(False)
 
-# TODO: change the title name to include your name
-plt.title("plot for mammal_teeth dataset")
+# Change the title name to include your name
+plt.title("Plot for mammal_teeth dataset - Bariah")
 plt.savefig("mammal_teeth_scatterplot.png", dpi=150) # save the figure
